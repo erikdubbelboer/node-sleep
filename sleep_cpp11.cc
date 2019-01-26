b67e63a5e0f042b56383c7819a15736e20d32bf1
Erik Dubbelboer <erik@dubbelboer.com>
2019-01-26 16:30:26 +0000

    Looks like sleep_for can also get interrupted

--- sleep_cpp11.cc
+++ sleep_cpp11.cc
@@ -6,7 +6,15 @@
 #include <chrono>
 
 void node_usleep(uint32_t usec) {
-  std::this_thread::sleep_for(std::chrono::microseconds(usec));
+  auto end = std::chrono::steady_clock::now() + std::chrono::microseconds(usec);
+
+  while (true) {
+    auto now = std::chrono::steady_clock::now();
+    if (now >= end) {
+      break;
+    }
+    std::this_thread::sleep_for(end - now);
+  }
 }
 
 #endif
